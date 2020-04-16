80aa98cb2805bacc8b8de0e61d5a82ee53921f2f
vancouver29 <vancouver2990@gmail.com>
2020-04-16 20:06:06 +0200

    created project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Quizzer</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>PHP Quizzer</h1>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <h2>Test Your PHP Knowledge</h2>
+            <p>This is a multiple choice quiz to test your knownedge of PHP</p>
+            <ul>
+                <li><strong>Number of Questions: </strong>5</li>
+                <li><strong>Type: </strong>Multiple Choice</li>
+                <li><strong>Estimated Time: </strong>4 Minutes</li>
+            </ul>
+            <a href="question.php?n=1" class="start">Start Quiz</a>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            Copyright &copy; 2020, PHP Quizzer
+        </div>
+    </footer>
+</body>
+</html>
